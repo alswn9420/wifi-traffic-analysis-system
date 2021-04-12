11df2087f209debe38cb63940033d38cb5ef602f
alswn9420 <60457578+alswn9420@users.noreply.github.com>
2021-04-12 23:38:46 +0900

    라이센스 추가

--- preprocess_tool/2_ProcessSession.ps1
+++ preprocess_tool/2_ProcessSession.ps1
@@ -1,8 +1,4 @@
-# AUTHOR:
-#   Wei Wang (ww8137@mail.ustc.edu.cn)
-# CONTRIBUTOR:
-#   David Lu (yungshenglu1994@gmail.com)
-#
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file, You
 # can obtain one at http://mozilla.org/MPL/2.0/.
